7b40ae5576494c511efa3d200cdd7beebdd0a187
Julian <jutanke@googlemail.com>
2013-10-16 13:36:01 +0200

    framework aufgabe 1 c

---
+++ src/ueb01/Test.java
@@ -0,0 +1,20 @@
+package ueb01;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Julian
+ * Date: 16.10.13
+ * Time: 13:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    public static void main(String[]args){
+
+        StringBufferImpl buffer = new StringBufferImpl();
+
+        // test me! Aufgabe b
+
+    }
+
+}
