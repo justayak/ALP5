c80f2e6590be0add3720f10a89ac284a34ed515b
Julian <jutanke@googlemail.com>
2013-10-21 15:42:24 +0200

    ueb1 - impl monitor

--- src/ueb01/Test.java
+++ src/ueb01/Test.java
@@ -1,5 +1,7 @@
 package ueb01;
 
+import java.io.IOException;
+
 /**
  * http://w3.inf.fu-berlin.de/lehre/WS13/alp5/uebungen/blatt1/index.html
  * Created with IntelliJ IDEA.
@@ -10,12 +12,21 @@ package ueb01;
  */
 public class Test {
 
-    public static void main(String[]args){
+    public static void main(String[]args) throws IOException, InterruptedException {
 
         StringBufferImpl buffer = new StringBufferImpl();
 
-        // test me! Aufgabe b
+        buffer.send("Hallo");
 
+        System.out.println(buffer.recv());
+
+        buffer.send("1");
+        buffer.send("2");
+        buffer.send("3");
+
+        System.out.println(buffer.recv());
+        System.out.println(buffer.recv());
+        System.out.println(buffer.recv());
     }
 
 }
