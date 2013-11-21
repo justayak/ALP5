b2c3374e22338ca3986c6d7dc7a26bf66a96e573
justayak <justayak@googlemail.com>
2013-11-21 01:07:43 +0100

    impl start for 4.2

--- src/ueb04/a2/Comm2.java
+++ src/ueb04/a2/Comm2.java
@@ -14,7 +14,28 @@ import java.util.List;
  */
 public class Comm2 {
 
-    private static HashMap<String, Integer> NameToPort = new HashMap<String, Integer>();
+    private static RoutingTable Routing = null;
+
+    public static void init(String name, int[] peers){
+
+        // sind wir der erste Knoten?
+        if (!available(FirstPort)){  // zugegeben: das ist ein schwaches Argument..
+
+            // Wir sind der erste Knoten!
+            Routing = new RoutingTable(name);
+
+        } else{
+
+            // Wir sind nicht der erste Knoten!
+            // finde die anderen und update das Netzwerk
+
+        }
+
+    }
+
+    /*================================================
+    N E T W O R K  M E T H O D S
+     ================================================*/
 
     /**
      * Sendet eine Nachricht über das Netzwerk an das Ziel
@@ -36,6 +57,38 @@ public class Comm2 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * http://svn.apache.org/viewvc/camel/trunk/components/camel-test/src/main/java/org/apache/camel/test/AvailablePortFinder.java?view=markup#l130
+     * Checks to see if a specific port is available.
+     *
+     * @param port the port to check for availability
+     */
+    public static boolean available(int port) {
+        ServerSocket ss = null;
+        DatagramSocket ds = null;
+        try {
+            ss = new ServerSocket(port);
+            ss.setReuseAddress(true);
+            ds = new DatagramSocket(port);
+            ds.setReuseAddress(true);
+            return true;
+        } catch (IOException e) {
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    /* should not be thrown */
+                }
+            }
+        }
+        return false;
     }
 
 
@@ -67,7 +120,77 @@ public class Comm2 {
         }
 
         public RoutingTable(List<Message> messages, String name){
+            List<RoutingTable> peers = new ArrayList<RoutingTable>();
+            for(Message m : messages){
+                peers.add(new RoutingTable(m));
+            }
+            RoutingTable o = peers.get(0);
+            if(peers.size() == 1){
+                for(int i = 0; i < o.length();i++){
+                    this.distances[i] = o.distance(i) + 1;
+                    this.ports[i] = o.port(i);
+                    this.names[i] = o.name(i);
+                    this.nameToPosition.put(o.name(i),i);
+                }
+            }else{
+                for(int i = 0; i < o.length();i++){
+                    RoutingTable lowest = o;
+                    for(RoutingTable t : peers){
+                        if(t.distance(i) < lowest.distance(i)){
+                            lowest = t;
+                        }
+                    }
+                    this.distances[i] = lowest.distance(i) + 1;
+                    this.ports[i] = lowest.port(i);
+                    this.names[i] = lowest.name(i);
+                    this.nameToPosition.put(lowest.name(i),i);
+                }
+            }
+        }
 
+        private RoutingTable(Message m){
+            String[] t = m.Message.split(";");
+            this.distances = new int[t.length];
+            this.ports = new int[t.length];
+            this.names = new String[t.length];
+            for(int i =0; i < t.length; i++){
+                String[]r = t[i].split(",");
+                this.distances[i] = Integer.parseInt(r[2]);
+                this.ports[i] = Integer.parseInt(r[1]);
+                this.names[i] = r[0];
+                this.nameToPosition.put(r[0],i);
+            }
+        }
+
+        public int length(){
+            return this.distances.length;
+        }
+
+        public int distance(int i){
+            return this.distances[i];
+        }
+
+        public int port(int i){
+            return this.ports[i];
+        }
+
+        public String name(int i){
+            return this.names[i];
+        }
+
+        public int distance(String name){
+            int i = this.nameToPosition.get(name);
+            return this.distances[i];
+        }
+
+        public int port(String name){
+            int i = this.nameToPosition.get(name);
+            return this.ports[i];
+        }
+
+        public String name(String name){
+            int i = this.nameToPosition.get(name);
+            return this.names[i];
         }
 
         @Override
@@ -165,6 +288,14 @@ public class Comm2 {
 
             }
         }
+
+        public Message(int port, String name, RoutingTable t){
+            this.Port = port;
+            this.Name = name;
+            this.Type = MessageType.ConnectAccept;
+            this.Message = t.toString();
+        }
+
         public Message(int port, String name,String message){
             this.Port = port;
             this.Name = name;
@@ -189,7 +320,7 @@ public class Comm2 {
         public String toString(){
             switch (this.Type){
                 case ConnectAccept:
-                    return "";
+                    return "3|" + this.Port + "|" + this.Name + "|"  + this.Message;
                 case Connect:
                     return "2|" + this.Port + "|" + this.Name;
                 case Update:
