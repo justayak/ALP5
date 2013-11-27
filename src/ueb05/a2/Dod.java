c40e32a1bf3999cac691aec53488e1d1ce1ffc7c
justayak <justayak@googlemail.com>
2013-11-27 18:44:36 +0100

    aufgabe 5.2

--- src/ueb05/a2/Dod.java
+++ src/ueb05/a2/Dod.java
@@ -1,5 +1,7 @@
 package ueb05.a2;
 
+import utils.Utils;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -10,7 +12,6 @@ import java.util.Scanner;
 import static utils.Utils.*;
 
 /**
- * Created with IntelliJ IDEA.
  * User: Julian
  * Date: 27.11.13
  * Time: 12:35
@@ -25,13 +26,15 @@ public class Dod {
         try {
             serverSocket = new ServerSocket(PORT);
             serverSocket.setSoTimeout(TIMEOUT);
-            Socket client = serverSocket.accept();
-            handleConnection(client);
+            while (true){
+                Socket client = serverSocket.accept();
+                handleConnection(client);
+            }
         } catch (IOException e) {
             System.out.println("hm, gab wohl nen Timeout.. (" + TIMEOUT + ") Millis!");
             e.printStackTrace();
-        } finally{
-            if (serverSocket != null){
+        } finally {
+            if (serverSocket != null) {
                 serverSocket.close();
             }
         }
@@ -39,29 +42,44 @@ public class Dod {
 
     /**
      * Behandlung bei Verbindung mit Client
+     *
      * @param client
      * @throws IOException
      */
-    private static void handleConnection(Socket client) throws IOException {
+    private static void handleConnection(final Socket client) throws IOException {
 
-        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-        out.println("was geht?? Timo und Julians Server hier! Mach mal was:");
+        new Thread() {
+            public void run() {
+                try {
+                    boolean isRunning = true;
+                    PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                    out.println("was geht?? Timo und Julians Server hier! Mach mal was:");
+                    while (isRunning) {
+                        Scanner in = new Scanner(client.getInputStream());
+                        String command = in.nextLine();
+                        System.out.println("command: " + command);
+                        String[] l = command.split(" ");
+                        String main = l[0];
+                        command = Utils.join(Arrays.copyOfRange(l, 1, l.length), " ");
+                        Process p = fork(main);
+                        send(p, command);
+                        String result = read(p);
+                        System.out.println(result);
+                        out.println(result);
+                        out.println("go on? (y or n)");
+                        isRunning = (in.nextLine().charAt(0) == 'y');
+                    }
 
-        Scanner in = new Scanner(client.getInputStream());
-
-        String command = in.nextLine();
-        System.out.println("command: " + command);
-
-        String[] l = command.split(" ");
-        String main = l[0];
-        command = join(Arrays.copyOfRange(l,1,l.length), " ");
-
-        Process p = fork(main);
-        send(p, command);
-        String result = read(p);
-        System.out.println(result);
-
-        out.println(result);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        client.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.run();
     }
-
 }
